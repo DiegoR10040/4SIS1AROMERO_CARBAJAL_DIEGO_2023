a2ba01c6293ab5c51c7b056542bc846cc2012a03
DiegoR10040 <d.romero.r235@edu.utc.mx>
2023-11-06 20:14:40 -0600

    Herencia

---
+++ Herencia/Principal.java
@@ -0,0 +1,10 @@
+package Herencia;
+
+class Principal {
+    // el metodo main
+    public static void main(String[] args){
+        //Hago la instancia de clase figura
+        Animal mascotas = new Animal();
+        mascotas.menu();
+}
+}
